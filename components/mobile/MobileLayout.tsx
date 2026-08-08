ed35618ae16c18c0bb1392f03fe039328a6072df
uunivpt <uunivpt@gmail.com>
2026-08-08 21:14:38 +0530

    Refine mobile shell for Theon concept UI

--- components/mobile/MobileLayout.tsx
+++ components/mobile/MobileLayout.tsx
@@ -8,22 +8,16 @@ type Props = {
 
 export default function MobileLayout({ children }: Props) {
   return (
-    <div className="md:hidden relative h-screen w-screen overflow-hidden bg-[#05060B] text-white">
-      {/* Background */}
-      <div className="absolute inset-0">
-
-        <div className="absolute -top-40 -left-24 h-80 w-80 rounded-full bg-violet-600/30 blur-[120px]" />
-
-        <div className="absolute bottom-0 right-0 h-72 w-72 rounded-full bg-cyan-500/20 blur-[120px]" />
-
-        <div className="absolute top-1/2 left-1/2 h-72 w-72 -translate-x-1/2 -translate-y-1/2 rounded-full bg-violet-500/10 blur-[100px]" />
-
+    <div className="md:hidden relative h-[100dvh] w-screen overflow-hidden bg-[#03030a] text-white">
+      <div className="pointer-events-none absolute inset-0 overflow-hidden">
+        <div className="absolute -left-24 -top-28 h-72 w-72 rounded-full bg-violet-700/25 blur-[110px]" />
+        <div className="absolute -right-24 top-1/3 h-64 w-64 rounded-full bg-cyan-600/10 blur-[120px]" />
+        <div className="absolute bottom-[-10%] left-1/3 h-80 w-80 -translate-x-1/2 rounded-full bg-violet-700/10 blur-[120px]" />
       </div>
 
-      <div className="relative z-10 flex h-full flex-col">
+      <div className="relative z-10 flex h-full min-h-0 flex-col">
         {children}
       </div>
-
     </div>
   );
 }
